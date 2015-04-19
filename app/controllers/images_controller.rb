d6ae652a004b009e36ea24a27497c1f67cd11442
Michał Gutowski <m.gutowski@binarapps.com>
2015-04-19 17:08:32 +0200

    base app working

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -70,6 +70,6 @@ class ImagesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def image_params
-      params.require(:image).permit(:title)
+      params.require(:image).permit(:title, :image)
     end
 end
